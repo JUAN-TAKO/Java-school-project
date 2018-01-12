adb400d628790368e49359caaa19d1d42a896102
JUAN TAKO <juan.tako.dev@gmail.com>
2018-01-12 14:49:23 +0100

    Merge origin/master
    Conflicts:
    	Vues/VueParametres.java

--- Generateurs/GrilleAleatoire.java
+++ Generateurs/GrilleAleatoire.java
@@ -12,53 +12,35 @@ package Generateurs;
 import java.util.ArrayList;
 import Utils.*;
 import Modele.*;
+import java.util.Arrays;
 import java.util.Collections;
+import java.util.HashSet;
 
 public class GrilleAleatoire extends Generateur{
     
     private ArrayList<TypeTuile> tuilesMelangees;
-    private TypeTuile[] tuilesValeurs;
-    private int[] tuilesInterdites = {0,1,4,5,6,11,24,29,30}; //valeur de tuiles nulles
+    private HashSet<Integer> tuilesInterdites; //indexes des tuiles nulles
     
     public void generate(Grille g){
         
         g.setSize(6);
-//        tuilesInterdites = new int[9];
-        tuilesValeurs = new TypeTuile[24]; 
-        tuilesValeurs = TypeTuile.values();    //on recupere les differents types de tuile dans un tableau (obligatoirement car la methode .values() renvoie un tableau
-        recupTuiles(); //on met ces types dans une arraylist
-        Collections.shuffle(tuilesMelangees);  //on melange les types
-        
-        for(int i = 0 ; i < 36 ; i++){        //parcourt la grille pour la remplir
-          for(int j = 0 ; j < 9 ; j++){       //parcourt la liste de tuiles interdites
-              if(i == tuilesInterdites[j]){   //si la tuile est interdite on l'instancie en tant que tel avec la methode addEmpty
-                  g.addEmpty();        
-              }
-              else{                           //sinon on ajoute une tuile d'un certain type à la grille, on retire cette tuile de la liste puis on re mélange la liste  
-                  
-                g.add(tuilesMelangees.get(0), Etat.SECHE);   
-                tuilesMelangees.remove(0);
-                Collections.shuffle(tuilesMelangees);
-                 
-              }
+        tuilesInterdites = new HashSet(Arrays.asList(0,1,4,5,6,11,24,29,30));
+        tuilesMelangees = new ArrayList<>(Arrays.asList(TypeTuile.values())); //on recupere les differents types de tuile dans un tableau (obligatoirement car la methode .values() renvoie un tableau
+        Collections.shuffle(tuilesMelangees);  //on melange les tuiles
+        int indexMelangees = 0;
+        for(int i = 0 ; i < g.getLength() ; i++){        //parcourt la grille pour la remplir
+          if(tuilesInterdites.contains(i)){   //si la tuile est interdite on l'instancie en tant que tel avec la methode addEmpty
+            g.addEmpty();        
+          }
+          else{     //sinon on ajoute une tuile d'un certain type à la grille, on retire cette tuile de la liste puis on re mélange la liste  
+            g.add(tuilesMelangees.get(indexMelangees), Etat.SECHE);   
+            Collections.shuffle(tuilesMelangees);
+            indexMelangees++;
           }
         }   
                 
            
         
-        
-    }
-    
-    public void recupTuiles(){
-       
-            for(int i = 0 ; i < 24 ; i++){
-                 for(TypeTuile t : tuilesValeurs){
-                 
-                     tuilesMelangees.add(t);
-                 
-                 }    
-            }
-           
         
     }
     
